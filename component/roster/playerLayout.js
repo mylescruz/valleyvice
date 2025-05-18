710a08355d7973580e846a87630b80aab09bb66f
Myles <mylesvcruz@gmail.com>
2025-05-17 18:18:24 -0700

    Update player layout in roster to make sure the page renders correctly

--- component/roster/playerLayout.js
+++ component/roster/playerLayout.js
@@ -12,11 +12,12 @@ const PlayerLayout = ({ playerId }) => {
 
   useEffect(() => {
     if (!rosterLoading && roster) {
-      setPlayer(roster?.find((player) => player.id === playerId));
+      const foundPlayer = roster?.find((player) => player.id === playerId);
+
+      setPlayer(foundPlayer);
     }
   }, [roster, rosterLoading, playerId]);
 
-  console.log(player);
   const playerAverages = {
     ppg: (
       (player.totalStats?.pm2 * 2 +
@@ -33,58 +34,59 @@ const PlayerLayout = ({ playerId }) => {
   const averagesTitleStyling = "text-xl text-(--secondary) font-bold";
   const averagesValueStyling = "font-bold";
 
-  if (rosterLoading) {
+  if (rosterLoading || !roster || !player) {
     return <LoadingIndicator />;
-  }
+  } else {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="w-11/12 sm:w-4/5">
+          <div className="flex flex-col lg:flex-row lg:items-center">
+            <div className="my-4 flex flex-col items-center lg:flex-row lg:justify-start">
+              <Image
+                src={`/images/${player.imageSrc}`}
+                alt="Player Image"
+                width={500}
+                height={500}
+                priority={true}
+                className="w-[250px] rounded-full shadow shadow-gray-500 lg:mr-8"
+              />
+              <div className="flex flex-col mt-4 font-bold">
+                <p className="text-3xl">
+                  <span className="text-(--primary)">#{player.number}</span>{" "}
+                  {player.name}
+                </p>
 
-  return (
-    <div className="flex flex-col items-center">
-      <div className="w-11/12 sm:w-4/5">
-        <div className="flex flex-col lg:flex-row lg:items-center">
-          <div className="my-4 flex flex-col items-center lg:flex-row lg:justify-start">
-            <Image
-              src={`/images/${player.imageSrc}`}
-              alt="Player Image"
-              width={500}
-              height={500}
-              className="w-[250px] rounded-full shadow shadow-gray-500 lg:mr-8"
-            />
-            <div className="flex flex-col mt-4 font-bold">
-              <p className="text-3xl">
-                <span className="text-(--primary)">#{player.number}</span>{" "}
-                {player.name}
-              </p>
-
-              <div className="text-(--secondary) text-center lg:text-start">
-                <p className="text-xl">{player.height}</p>
-                <p className="text-lg">{player.position}</p>
+                <div className="text-(--secondary) text-center lg:text-start">
+                  <p className="text-xl">{player.height}</p>
+                  <p className="text-lg">{player.position}</p>
+                </div>
+              </div>
+            </div>
+            <div className="mb-4 flex flex-row flex-wrap justify-evenly border-2 border-(--primary) w-full rounded-lg px-2 p-1 md:w-2/3 md:mx-auto lg:w-1/2 xl:w-1/3">
+              <div className={averagesContainer}>
+                <p className={averagesTitleStyling}>PPG</p>
+                <p className={averagesValueStyling}>{playerAverages.ppg}</p>
+              </div>
+              <div className={averagesContainer}>
+                <p className={averagesTitleStyling}>REB</p>
+                <p className={averagesValueStyling}>{playerAverages.reb}</p>
+              </div>
+              <div className={averagesContainer}>
+                <p className={averagesTitleStyling}>AST</p>
+                <p className={averagesValueStyling}>{playerAverages.ast}</p>
+              </div>
+              <div className={averagesContainer}>
+                <p className={averagesTitleStyling}>STL</p>
+                <p className={averagesValueStyling}>{playerAverages.stl}</p>
               </div>
             </div>
           </div>
-          <div className="mb-4 flex flex-row flex-wrap justify-evenly border-2 border-(--primary) w-full rounded-lg px-2 p-1 md:w-2/3 md:mx-auto lg:w-1/2 xl:w-1/3">
-            <div className={averagesContainer}>
-              <p className={averagesTitleStyling}>PPG</p>
-              <p className={averagesValueStyling}>{playerAverages.ppg}</p>
-            </div>
-            <div className={averagesContainer}>
-              <p className={averagesTitleStyling}>REB</p>
-              <p className={averagesValueStyling}>{playerAverages.reb}</p>
-            </div>
-            <div className={averagesContainer}>
-              <p className={averagesTitleStyling}>AST</p>
-              <p className={averagesValueStyling}>{playerAverages.ast}</p>
-            </div>
-            <div className={averagesContainer}>
-              <p className={averagesTitleStyling}>STL</p>
-              <p className={averagesValueStyling}>{playerAverages.stl}</p>
-            </div>
-          </div>
+          <SeasonAverages playerId={player.id} />
+          <SeasonTotals playerId={player.id} />
         </div>
-        <SeasonAverages playerId={player.id} />
-        <SeasonTotals playerId={player.id} />
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PlayerLayout;
--- component/roster/seasonTotalsRows.js
+++ component/roster/seasonTotalsRows.js
@@ -8,53 +8,56 @@ const SeasonTotalsRows = ({ playerId, seasonNumber, totalCellsStyle }) => {
 
   useEffect(() => {
     if (!seasonLoading && season) {
-      setStats(
-        season.playerTotalStats.find((player) => player.id === playerId)
+      const playerTotals = season.playerTotalStats.find(
+        (player) => player.id === playerId
       );
+
+      setStats({
+        ...playerTotals,
+        pts: playerTotals.pm2 * 2 + playerTotals.pm3 * 3 + playerTotals.ft,
+        pct2:
+          playerTotals.pa2 === 0
+            ? `0%`
+            : `${((playerTotals.pm2 / playerTotals.pa2) * 100).toFixed(0)}%`,
+        pct3:
+          playerTotals.pa2 === 0
+            ? `0%`
+            : `${((playerTotals.pm3 / playerTotals.pa3) * 100).toFixed(0)}%`,
+        pctft:
+          playerTotals.fta === 0
+            ? `0%`
+            : `${((playerTotals.ft / playerTotals.fta) * 100).toFixed(0)}%`,
+      });
     }
   }, [season, seasonLoading, playerId]);
 
   if (seasonLoading) {
     return <></>;
+  } else {
+    return (
+      <tr>
+        <td className={totalCellsStyle}>{seasonNumber}</td>
+        <td className={totalCellsStyle}>{stats.gp}</td>
+        <td className={totalCellsStyle}>{stats.pts}</td>
+        <td className={totalCellsStyle}>{stats.pm2}</td>
+        <td className={totalCellsStyle}>{stats.pa2}</td>
+        <td className={totalCellsStyle}>{stats.pct2}</td>
+        <td className={totalCellsStyle}>{stats.pm3}</td>
+        <td className={totalCellsStyle}>{stats.pa3}</td>
+        <td className={totalCellsStyle}>{stats.pct3}</td>
+        <td className={totalCellsStyle}>{stats.ft}</td>
+        <td className={totalCellsStyle}>{stats.fta}</td>
+        <td className={totalCellsStyle}>{stats.pctft}</td>
+        <td className={totalCellsStyle}>{stats.reb}</td>
+        <td className={totalCellsStyle}>{stats.ast}</td>
+        <td className={totalCellsStyle}>{stats.stl}</td>
+        <td className={totalCellsStyle}>{stats.blk}</td>
+        <td className={totalCellsStyle}>{stats.to}</td>
+        <td className={totalCellsStyle}>{stats.pf}</td>
+        <td className={totalCellsStyle}>{stats.ckd}</td>
+      </tr>
+    );
   }
-
-  return (
-    <tr>
-      <td className={totalCellsStyle}>{seasonNumber}</td>
-      <td className={totalCellsStyle}>{stats.gp}</td>
-      <td className={totalCellsStyle}>
-        {stats.pm2 * 2 + stats.pm3 * 3 + stats.ft}
-      </td>
-      <td className={totalCellsStyle}>{stats.pm2}</td>
-      <td className={totalCellsStyle}>{stats.pa2}</td>
-      <td className={totalCellsStyle}>
-        {stats.pa2 === 0
-          ? `0%`
-          : `${((stats.pm2 / stats.pa2) * 100).toFixed(0)}%`}
-      </td>
-      <td className={totalCellsStyle}>{stats.pm3}</td>
-      <td className={totalCellsStyle}>{stats.pa3}</td>
-      <td className={totalCellsStyle}>
-        {stats.pa3 === 0
-          ? `0%`
-          : `${((stats.pm3 / stats.pa3) * 100).toFixed(0)}%`}
-      </td>
-      <td className={totalCellsStyle}>{stats.ft}</td>
-      <td className={totalCellsStyle}>{stats.fta}</td>
-      <td className={totalCellsStyle}>
-        {stats.fta === 0
-          ? `0%`
-          : `${((stats.ft / stats.fta) * 100).toFixed(0)}%`}
-      </td>
-      <td className={totalCellsStyle}>{stats.reb}</td>
-      <td className={totalCellsStyle}>{stats.ast}</td>
-      <td className={totalCellsStyle}>{stats.stl}</td>
-      <td className={totalCellsStyle}>{stats.blk}</td>
-      <td className={totalCellsStyle}>{stats.to}</td>
-      <td className={totalCellsStyle}>{stats.pf}</td>
-      <td className={totalCellsStyle}>{stats.ckd}</td>
-    </tr>
-  );
 };
 
 export default SeasonTotalsRows;
