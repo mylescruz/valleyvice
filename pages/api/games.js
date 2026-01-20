c5eefd4fc7670d8c686b2db01f0a573cbcf8aaba
Myles <mylesvcruz@gmail.com>
2026-01-20 17:38:05 -0600

    refactor: games: Add a mongoDB session with transactions when completing a game so that if any error occurs, the game will not be saved and cause duplicate data on re-attempt to complete the game

--- pages/api/games.js
+++ pages/api/games.js
@@ -23,6 +23,8 @@ export default async function handler(req, res) {
   const playersCol = db.collection("players");
 
   if (method === "POST") {
+    const mongoSession = await client.startSession();
+
     try {
       const game = req?.body;
 
@@ -37,19 +39,19 @@ export default async function handler(req, res) {
         const twoPointPercentage =
           player.twoPointsAttempted !== 0
             ? Math.round(
-                (player.twoPointsMade / player.twoPointsAttempted) * 100
+                (player.twoPointsMade / player.twoPointsAttempted) * 100,
               )
             : 0;
         const threePointPercentage =
           player.threePointsAttempted !== 0
             ? Math.round(
-                (player.threePointsMade / player.threePointsAttempted) * 100
+                (player.threePointsMade / player.threePointsAttempted) * 100,
               )
             : 0;
         const freeThrowPercentage =
           player.freeThrowsAttempted !== 0
             ? Math.round(
-                (player.freeThrowsMade / player.freeThrowsAttempted) * 100
+                (player.freeThrowsMade / player.freeThrowsAttempted) * 100,
               )
             : 0;
 
@@ -77,172 +79,193 @@ export default async function handler(req, res) {
         };
       });
 
-      const insertGameResult = await gamesCol.insertOne({
-        ...finalGame,
-        players: updatedPlayers,
-      });
-      console.log(insertGameResult);
+      // Start a transaction to process all MongoDB statements or rollback any failures
+      await mongoSession.withTransaction(async (session) => {
+        const insertGameResult = await gamesCol.insertOne(
+          {
+            ...finalGame,
+            players: updatedPlayers,
+          },
+          { session },
+        );
+        console.log(insertGameResult);
 
-      // Update the season with the results of the new game
-      let updateStatement = {
-        $inc: {
-          gamesPlayed: 1,
-        },
-      };
+        // Update the season with the results of the new game
+        let updateStatement = {
+          $inc: {
+            gamesPlayed: 1,
+          },
+        };
 
-      if (finalGame.result === "W") {
-        updateStatement.$inc.wins = 1;
-      } else if (finalGame.result === "L") {
-        updateStatement.$inc.losses = 1;
-      }
+        if (finalGame.result === "W") {
+          updateStatement.$inc.wins = 1;
+        } else if (finalGame.result === "L") {
+          updateStatement.$inc.losses = 1;
+        }
 
-      const seasonUpdateResult = await seasonsCol.updateOne(
-        { seasonNumber: game.seasonNumber },
-        updateStatement
-      );
-      console.log(seasonUpdateResult);
+        const seasonUpdateResult = await seasonsCol.updateOne(
+          { seasonNumber: game.seasonNumber },
+          updateStatement,
+          { session },
+        );
+        console.log(seasonUpdateResult);
 
-      // Update the players' stats in MongoDB
-      await Promise.all(
-        game.players.map(async (player) => {
-          const currentPlayer = await playersCol.findOne({
-            playerId: player.playerId,
-          });
-
-          const currentSeason = currentPlayer.seasons.find(
-            (season) => season.seasonNumber === game.seasonNumber
-          );
-
-          if (currentSeason) {
-            const totalTwoPointAttempts =
-              player.twoPointsAttempted + currentSeason.twoPointsAttempted;
-            const twoPointPercentage =
-              totalTwoPointAttempts !== 0
-                ? Math.round(
-                    ((currentSeason.twoPointsMade + player.twoPointsMade) /
-                      totalTwoPointAttempts) *
-                      100
-                  )
-                : "0";
-
-            const totalThreePointAttempts =
-              player.threePointsAttempted + currentSeason.threePointsAttempted;
-            const threePointPercentage =
-              totalThreePointAttempts !== 0
-                ? Math.round(
-                    ((currentSeason.threePointsMade + player.threePointsMade) /
-                      totalThreePointAttempts) *
-                      100
-                  )
-                : "0";
-
-            const totalFreeThrowAttempts =
-              player.freeThrowsAttempted + currentSeason.freeThrowsAttempted;
-            const freeThrowPercentage =
-              totalFreeThrowAttempts !== 0
-                ? Math.round(
-                    ((currentSeason.freeThrowsMade + player.freeThrowsMade) /
-                      totalFreeThrowAttempts) *
-                      100
-                  )
-                : "0";
-
-            await playersCol.updateOne(
+        // Update the players' stats in MongoDB
+        await Promise.all(
+          game.players.map(async (player) => {
+            const currentPlayer = await playersCol.findOne(
               {
                 playerId: player.playerId,
-                "seasons.seasonNumber": game.seasonNumber,
               },
-              {
-                $inc: {
-                  "seasons.$.gamesPlayed": 1,
-                  "seasons.$.points": player.points,
-                  "seasons.$.twoPointsMade": player.twoPointsMade,
-                  "seasons.$.twoPointsAttempted": player.twoPointsAttempted,
-                  "seasons.$.threePointsMade": player.threePointsMade,
-                  "seasons.$.threePointsAttempted": player.threePointsAttempted,
-                  "seasons.$.freeThrowsMade": player.freeThrowsMade,
-                  "seasons.$.freeThrowsAttempted": player.freeThrowsAttempted,
-                  "seasons.$.rebounds": player.rebounds,
-                  "seasons.$.assists": player.assists,
-                  "seasons.$.steals": player.steals,
-                  "seasons.$.blocks": player.blocks,
-                  "seasons.$.turnovers": player.turnovers,
-                  "seasons.$.personalFouls": player.personalFouls,
-                  "seasons.$.cooked": player.cooked,
-                },
-                $set: {
-                  "seasons.$.twoPointPercentage": twoPointPercentage,
-                  "seasons.$.threePointPercentage": threePointPercentage,
-                  "seasons.$.freeThrowPercentage": freeThrowPercentage,
-                },
-              }
+              { session },
             );
-          } else {
-            await playersCol.updateOne(
-              {
-                playerId: player.playerId,
-              },
-              {
-                $push: {
-                  seasons: {
-                    seasonNumber: game.seasonNumber,
-                    gamesPlayed: 1,
-                    points: player.points,
-                    twoPointsMade: player.twoPointsMade,
-                    twoPointsAttempted: player.twoPointsAttempted,
-                    twoPointPercentage:
-                      player.twoPointsAttempted !== 0
-                        ? Math.round(
-                            (player.twoPointsMade / player.twoPointsAttempted) *
-                              100
-                          )
-                        : 0,
-                    threePointsMade: player.threePointsMade,
-                    threePointsAttempted: player.threePointsAttempted,
-                    threePointPercentage:
-                      player.threePointsAttempted !== 0
-                        ? Math.round(
-                            (player.threePointsMade /
-                              player.threePointsAttempted) *
-                              100
-                          )
-                        : 0,
-                    freeThrowsMade: player.freeThrowsMade,
-                    freeThrowsAttempted: player.freeThrowsAttempted,
-                    freeThrowPercentage:
-                      player.freeThrowsAttempted !== 0
-                        ? Math.round(
-                            (player.freeThrowsMade /
-                              player.freeThrowsAttempted) *
-                              100
-                          )
-                        : 0,
-                    rebounds: player.rebounds,
-                    assists: player.assists,
-                    steals: player.steals,
-                    blocks: player.blocks,
-                    turnovers: player.turnovers,
-                    personalFouls: player.personalFouls,
-                    cooked: player.cooked,
+
+            const currentSeason = currentPlayer.seasons.find(
+              (season) => season.seasonNumber === game.seasonNumber,
+            );
+
+            if (currentSeason) {
+              const totalTwoPointAttempts =
+                player.twoPointsAttempted + currentSeason.twoPointsAttempted;
+              const twoPointPercentage =
+                totalTwoPointAttempts !== 0
+                  ? Math.round(
+                      ((currentSeason.twoPointsMade + player.twoPointsMade) /
+                        totalTwoPointAttempts) *
+                        100,
+                    )
+                  : "0";
+
+              const totalThreePointAttempts =
+                player.threePointsAttempted +
+                currentSeason.threePointsAttempted;
+              const threePointPercentage =
+                totalThreePointAttempts !== 0
+                  ? Math.round(
+                      ((currentSeason.threePointsMade +
+                        player.threePointsMade) /
+                        totalThreePointAttempts) *
+                        100,
+                    )
+                  : "0";
+
+              const totalFreeThrowAttempts =
+                player.freeThrowsAttempted + currentSeason.freeThrowsAttempted;
+              const freeThrowPercentage =
+                totalFreeThrowAttempts !== 0
+                  ? Math.round(
+                      ((currentSeason.freeThrowsMade + player.freeThrowsMade) /
+                        totalFreeThrowAttempts) *
+                        100,
+                    )
+                  : "0";
+
+              await playersCol.updateOne(
+                {
+                  playerId: player.playerId,
+                  "seasons.seasonNumber": game.seasonNumber,
+                },
+                {
+                  $inc: {
+                    "seasons.$.gamesPlayed": 1,
+                    "seasons.$.points": player.points,
+                    "seasons.$.twoPointsMade": player.twoPointsMade,
+                    "seasons.$.twoPointsAttempted": player.twoPointsAttempted,
+                    "seasons.$.threePointsMade": player.threePointsMade,
+                    "seasons.$.threePointsAttempted":
+                      player.threePointsAttempted,
+                    "seasons.$.freeThrowsMade": player.freeThrowsMade,
+                    "seasons.$.freeThrowsAttempted": player.freeThrowsAttempted,
+                    "seasons.$.rebounds": player.rebounds,
+                    "seasons.$.assists": player.assists,
+                    "seasons.$.steals": player.steals,
+                    "seasons.$.blocks": player.blocks,
+                    "seasons.$.turnovers": player.turnovers,
+                    "seasons.$.personalFouls": player.personalFouls,
+                    "seasons.$.cooked": player.cooked,
+                  },
+                  $set: {
+                    "seasons.$.twoPointPercentage": twoPointPercentage,
+                    "seasons.$.threePointPercentage": threePointPercentage,
+                    "seasons.$.freeThrowPercentage": freeThrowPercentage,
                   },
                 },
-              }
-            );
-          }
-        })
-      );
+                { session },
+              );
+            } else {
+              await playersCol.updateOne(
+                {
+                  playerId: player.playerId,
+                },
+                {
+                  $push: {
+                    seasons: {
+                      seasonNumber: game.seasonNumber,
+                      gamesPlayed: 1,
+                      points: player.points,
+                      twoPointsMade: player.twoPointsMade,
+                      twoPointsAttempted: player.twoPointsAttempted,
+                      twoPointPercentage:
+                        player.twoPointsAttempted !== 0
+                          ? Math.round(
+                              (player.twoPointsMade /
+                                player.twoPointsAttempted) *
+                                100,
+                            )
+                          : 0,
+                      threePointsMade: player.threePointsMade,
+                      threePointsAttempted: player.threePointsAttempted,
+                      threePointPercentage:
+                        player.threePointsAttempted !== 0
+                          ? Math.round(
+                              (player.threePointsMade /
+                                player.threePointsAttempted) *
+                                100,
+                            )
+                          : 0,
+                      freeThrowsMade: player.freeThrowsMade,
+                      freeThrowsAttempted: player.freeThrowsAttempted,
+                      freeThrowPercentage:
+                        player.freeThrowsAttempted !== 0
+                          ? Math.round(
+                              (player.freeThrowsMade /
+                                player.freeThrowsAttempted) *
+                                100,
+                            )
+                          : 0,
+                      rebounds: player.rebounds,
+                      assists: player.assists,
+                      steals: player.steals,
+                      blocks: player.blocks,
+                      turnovers: player.turnovers,
+                      personalFouls: player.personalFouls,
+                      cooked: player.cooked,
+                    },
+                  },
+                },
+                { session },
+              );
+            }
+          }),
+        );
 
-      // Update the player's stats for the given season
-      await updateSeasonStats(finalGame.seasonNumber);
+        // Update the player's stats for the given season
+        await updateSeasonStats({
+          seasonNumber: finalGame.seasonNumber,
+          session,
+        });
 
-      // Update the all time leader's rankings
-      await setAllTimeLeaders();
+        // Update the all time leader's rankings
+        await setAllTimeLeaders({ session });
+      });
 
       // Send retrieved game back to the client
       res.status(200).json(game);
     } catch (error) {
       console.error(`${method} game request failed: ${error}`);
       res.status(500).send("Error adding new game data");
+    } finally {
+      await mongoSession.endSession();
     }
   } else {
     res.status(405).send(`Method ${method} not allowed`);
