198b7cd088c9a1ae7d2d87581861fba1ed2febf4
Myles <mylesvcruz@gmail.com>
2026-01-20 22:02:14 -0600

    refactor: roster: Update the rosterLayout component to call the InnerRosterLayout after the info context has loaded succesfully to ensure smooth data fetching

--- component/roster/rosterLayout.js
+++ component/roster/rosterLayout.js
@@ -1,51 +1,50 @@
 import LoadingIndicator from "../layout/loadingIndicator";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import SeasonRoster from "./seasonRoster";
 import { InfoContext } from "@/contexts/InfoContext";
 import ErrorLayout from "../layout/errorLayout";
 
-const RosterLayout = () => {
-  const { info, infoLoading } = useContext(InfoContext);
-  const [seasonNumber, setSeasonNumber] = useState(null);
-
-  useEffect(() => {
-    if (!infoLoading && info) {
-      setSeasonNumber(info.currentSeason);
-    }
-  }, [info, infoLoading]);
+const InnerRosterLayout = ({ info }) => {
+  const [seasonNumber, setSeasonNumber] = useState(info.currentSeason);
 
   const selectSeason = (e) => {
     setSeasonNumber(e.target.value);
   };
 
-  if (infoLoading && !info && !seasonNumber) {
-    return <LoadingIndicator />;
-  } else if (info && seasonNumber) {
-    return (
-      <div className="flex flex-col items-center">
-        <div className="w-11/12 sm:w-4/5">
-          <div className="mt-2 mb-4 text-center">
-            <label htmlFor="season">Choose a season:</label>
-            <select
-              id="season"
-              className="px-2 border-1 border-(--secondary) rounded-lg mx-2 py-1"
-              onChange={selectSeason}
-              value={seasonNumber}
-            >
-              {info.seasonsPlayed.map((season) => (
-                <option key={season} value={season}>
-                  {season}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <SeasonRoster seasonNumber={seasonNumber} />
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-11/12 sm:w-4/5">
+        <div className="mt-2 mb-4 text-center">
+          <label htmlFor="season">Choose a season:</label>
+          <select
+            id="season"
+            className="px-2 border-1 border-(--secondary) rounded-lg mx-2 py-1"
+            onChange={selectSeason}
+            value={seasonNumber}
+          >
+            {info.seasonsPlayed.map((season) => (
+              <option key={season} value={season}>
+                {season}
+              </option>
+            ))}
+          </select>
         </div>
+
+        <SeasonRoster seasonNumber={seasonNumber} />
       </div>
-    );
-  } else {
+    </div>
+  );
+};
+
+const RosterLayout = () => {
+  const { info, infoLoading } = useContext(InfoContext);
+
+  if (infoLoading) {
+    return <LoadingIndicator />;
+  } else if (!info) {
     return <ErrorLayout />;
+  } else {
+    return <InnerRosterLayout info={info} />;
   }
 };
 
