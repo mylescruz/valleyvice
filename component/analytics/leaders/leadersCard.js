728d928427ede26a395933b2541acf6e3bad65ce
Myles <mylesvcruz@gmail.com>
2026-02-13 10:05:27 -0600

    feature: leadersCard: Include a stat leader's image in the card

--- component/analytics/leaders/leadersCard.js
+++ component/analytics/leaders/leadersCard.js
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 const LeadersCard = ({ stat }) => {
   return (
     <div className="border-2 border-(--secondary) rounded-lg m-2 px-4 py-4">
-      <div className="text-center font-bold">
+      <div className="flex flex-col items-center text-center font-bold">
         <h1 className="text-white text-2xl">{stat.stat}</h1>
+        <Image
+          src={`/images/${stat.imageSrc}`}
+          alt={stat.imageAlt}
+          height={100}
+          width={100}
+          className="rounded-full shadow shadow-gray-500"
+        />
         <h3 className="text-(--primary) text-xl">{stat.name}</h3>
         <h6 className="text-lg">{stat.value}</h6>
       </div>
