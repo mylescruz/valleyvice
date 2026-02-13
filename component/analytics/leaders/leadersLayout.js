40c5beb2d9193acf768d36f02dd3bedd06ef8c6a
Myles <mylesvcruz@gmail.com>
2026-02-13 09:36:08 -0600

    feature: leadersLayout: Remove the charts graphic for the average and total leaders and replace with cards for each individual stat

--- component/analytics/leaders/leadersLayout.js
+++ component/analytics/leaders/leadersLayout.js
@@ -1,16 +1,27 @@
-import AverageLeadersChart from "./averageLeadersChart";
-import TotalLeadersChart from "./totalLeadersChart";
+import LeadersCard from "./leadersCard";
 
 const LeadersLayout = ({ allTimeLeaders }) => {
   return (
     <div>
       <div className="flex flex-col items-center my-4">
-        <h1 className="text-center">Average Leaders</h1>
-        <AverageLeadersChart avgLeaders={allTimeLeaders.averageLeaders} />
+        <h1 className="text-3xl text-(--primary) font-bold mb-2">
+          Average Leaders
+        </h1>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7">
+          {allTimeLeaders.averageLeaders.map((stat) => (
+            <LeadersCard key={stat.id} stat={stat} />
+          ))}
+        </div>
       </div>
       <div className="flex flex-col items-center my-4">
-        <h1 className="text-center">Total Leaders</h1>
-        <TotalLeadersChart totalLeaders={allTimeLeaders.totalLeaders} />
+        <h1 className="text-3xl text-(--primary) font-bold mb-2">
+          Total Leaders
+        </h1>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7">
+          {allTimeLeaders.totalLeaders.map((stat) => (
+            <LeadersCard key={stat.id} stat={stat} />
+          ))}
+        </div>
       </div>
     </div>
   );
